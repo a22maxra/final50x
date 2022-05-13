adff3f99fdb035896425c41535cb423b25a1e4f8
Max6956 <max.rapp01@gmail.com>
2022-05-13 06:08:01 +0200

    to pc

---
+++ popup.js
@@ -0,0 +1,7 @@
+
+    document.querySelector('#saveButton').addEventListener('click', function(){
+        console.log("hello");
+        document.querySelector('.test').innerHTML = 'max@email.com';
+        console.log("hello2");
+    });
+
